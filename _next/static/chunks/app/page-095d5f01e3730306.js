7cf51e738bfb12caa90f290385c7fe304eb05ebf
lennykioko <lennykmutua@gmail.com>
2026-04-15 09:32:24 +0000

    Deploying to gh-pages from @ lennykioko/lennykioko.github.io@fa097958c233c2a7a99e4033d70436ae79f1fb23 🚀

---
+++ _next/static/chunks/app/page-095d5f01e3730306.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{8543:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[850,441,255,358],()=>e(e.s=8543)),_N_E=e.O()}]);
